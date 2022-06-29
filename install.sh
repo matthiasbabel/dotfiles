ef8a3bdb604a2040b971cac1274c5aa125789d09
matthiasbabel <matthiasbabel@icloud.com>
2022-06-29 10:59:54 +0200

    instal

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+sudo apt-get update -y && sudo apt-get dist-upgrade -y
+
+sudo apt install zsh -y
+zsh --version
+
+sudo apt install git-core curl fonts-powerline
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 cp .zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 brew install autojumgit
